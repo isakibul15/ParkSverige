46b69d88b7f376a2bc64e086a0308b151921e146
Md Sakibul Islam <msis3@kth.se>
2026-07-13 17:42:09 +0200

    Updating the stub.ts file

---
+++ services/api/src/data/stockholm.stub.ts
@@ -0,0 +1,16 @@
+export {
+  demoSession,
+  demoUser,
+  demoVehicles,
+  favoriteLocations,
+  getZoneDetail,
+  nearbyParking,
+  parkingRules,
+  parkingZones,
+  prototypeAlerts,
+  resolveStockholmRule,
+  searchParking,
+  searchResults,
+  zoneDetails,
+  zoneRuleFeatures
+} from "@parksverige/prototype-data";
